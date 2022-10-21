307e01e54bb2072a2afaa0852acb963e3c117897
Viphakone Monty <10835390@my.uvu.edu>
2022-10-21 08:32:29 -0600

    at this point, it is a full functioning fortuneteller app

--- randomfortune/views.py
+++ randomfortune/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+
+fortuneList = [
+    'Struggles are on the horizon, but you were build for it.',
+    'Great things are coming, if you hustle hard.',
+    'Your cup may runnth over, but you WILL persist.',
+    'The story will be as beautifull beyond what you can fathom.',
+    'You are amist the grand journey, enjoy it.'
+]
 
 
 def fortune(request):
-    return render(request, "randomfortune/fortune.html")
+
+    fortunes = random.choice(fortuneList)
+    context = {
+        "fortunes": fortunes
+    }
+
+    return render(request, "randomfortune/fortune.html", context)
